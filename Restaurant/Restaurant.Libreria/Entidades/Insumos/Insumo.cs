3fa80eb84bc0666b4b1fc5ac7a3f0d45a985881a
Valeria-Dominguez <valeriadominguez@outlook.com>
2021-03-24 16:38:02 -0300

    Restaurant
    Restaurant

--- Restaurant/Restaurant.Libreria/Entidades/Insumos/Insumo.cs
+++ Restaurant/Restaurant.Libreria/Entidades/Insumos/Insumo.cs
@@ -8,8 +8,8 @@ namespace Restaurant.Libreria.Entidades
 {
     public class Insumo
     {
-        public int IdInsumo { private set; get; }
-        public string NombreInsumo { private set; get; }
+        public int IdInsumo { set; get; }
+        public string NombreInsumo { set; get; }
         public string CategoriaInsumo { private set; get; }
         public float MedidaInsumo { private set; get; }
         public string UDeMedidaInsumo { private set; get; }
