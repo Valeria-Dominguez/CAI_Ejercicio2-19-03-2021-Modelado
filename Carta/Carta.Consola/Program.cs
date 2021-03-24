563fd445a2fb2750fb962328feb80e0a8ada1873
Valeria-Dominguez <valeriadominguez@outlook.com>
2021-03-24 19:31:18 -0300

    Carta
    Carta

---
+++ Carta/Carta.Consola/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Carta.Libreria.Entidades;
+
+namespace Carta.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mazo unMazo = new Mazo();
+            unMazo.MazoUnJuego[0].PaloCarta = "Basto";
+
+        }
+    }
+}
---
+++ Carta/Carta.Libreria/Entidades/Carta.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carta.Libreria
+{
+    public class Carta
+    {
+        private int idCarta;
+        private string nombreCarta;
+        private int numeroCarta;
+        private string paloCarta;
+
+        public int IdCarta
+        {
+            set
+            {
+                this.idCarta = value;
+            }
+            get
+            {
+                return idCarta;
+            }
+        }
+        public string NombreCarta
+        {
+            set
+            {
+                this.nombreCarta = value;
+            }
+            get
+            {
+                return nombreCarta;
+            }
+        }
+        public int NumeroCarta
+        {
+            set
+            {
+                this.numeroCarta = value;
+            }
+            get
+            {
+                return numeroCarta;
+            }
+        }
+        public string PaloCarta
+        {
+            set
+            {
+                this.paloCarta = value;
+            }
+            get
+            {
+                return paloCarta;
+            }
+        }
+
+    }
+}
