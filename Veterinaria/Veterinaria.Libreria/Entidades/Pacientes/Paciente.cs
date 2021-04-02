6858376d4e7a122effd5b9fdfacb79deef6d68f6
Valeria-Dominguez <valeriadominguez@outlook.com>
2021-04-02 13:00:45 -0300

    Veterinaria con variables
    Veterinaria con variables

--- Veterinaria/Veterinaria.Libreria/Entidades/Pacientes/Paciente.cs
+++ Veterinaria/Veterinaria.Libreria/Entidades/Pacientes/Paciente.cs
@@ -13,20 +13,14 @@ namespace Veterinaria.Libreria.Entidades
         public string EspeciePaci { private set; get; }
         public string EdadPaci { private set; get; }
         public string NombreDueño { private set; get; }
-
+        const int cantMaxVisitas = 200;
+        public Visita[] Visitas { private set; get; }
         const int cantMaxAntecedentes = 50;
         public string[] AntecedentesPaci { private set; get; } = new string[cantMaxAntecedentes];
 
-        const int cantMaxVisitas = 200;
-        const int cantColumVisitas = 6;
-        const int columFechaVisita = 0;
-        const int columMotivoConsulta = 1;
-        const int columDiagnostico = 2;
-        const int columPrescripciones = 3;
-        const int columObservaciones = 4;
-        const int columProfesional = 5;
+       
 
-        public string[,] VisitasPaci { private set; get; } = new string[cantMaxVisitas,cantColumVisitas];
+        
 
 
 
---
+++ Veterinaria/Veterinaria.Libreria/Entidades/Pacientes/Visita.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria.Libreria.Entidades
+{
+    public class Visita
+    {
+        public string FechaVisita { private set; get; }
+        public string MotivoConsulta { private set; get; }
+        public string Diagnostico { private set; get; }
+        public string Prescripciones { private set; get; }
+        public string Observaciones { private set; get; }
+        public string NombreProfesional { private set; get; }        
+    }
+}
